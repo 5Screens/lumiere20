d1177191a60f172f7c4fc363a481200fa5f71e84
Marc Oliva <marc.oliva@we-it.fr>
2025-03-27 20:30:53 +0100

    BE - Augmentation de la taille des requêtes à 50Mb

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,8 @@ const ticketsRoutes = require('./api/v1/tickets/routes');
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Augmentation de la taille des requêtes à 50MB
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Logging middleware
 app.use((req, res, next) => {
